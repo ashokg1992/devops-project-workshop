68813b5edd35065a6bf24953f95571eb0e417c5e
ashok <ashokreddyothers@gmail.com>
2023-07-19 18:34:08 +0530

    1

--- code/terraform1/vpc/v1-ec2.tf
+++ code/terraform1/vpc/v1-ec2.tf
@@ -7,5 +7,5 @@ resource "aws_instance" "demo-server" {
     ami = "ami-0f5ee92e2d63afc18"
     instance_type = "t3.small"
     key_name = "gashok120707"
-    subnet_id = aws_subnet.dpw-public_subent_01.id 
+    subnet_id = aws_subnet.default.id 
 }
