dfa910cdd5db8bfcce425289d121316850c8c012
ashok <ashokreddyothers@gmail.com>
2023-07-19 18:27:46 +0530

    1

--- code/terraform1/vpc/v1-ec2.tf
+++ code/terraform1/vpc/v1-ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = " ami-0f5ee92e2d63afc18"
+    ami = "ami-0f5ee92e2d63afc18"
     instance_type = "t3.small"
     key_name = "gashok120707"
 }
